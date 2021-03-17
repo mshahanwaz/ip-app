6b4496cfe873179e5462d2deb8e5e6f41676198a
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-03-17 22:19:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import Image from "image-js";
+import { useEffect } from 'react';
 
 function App() {
+  var value;
+  // execute().catch(console.error);
+  
+  async function execute() {
+    let image = await Image.load('1.jpg');
+    // let grey = image
+    //   .grey() // convert the image to greyscale.
+    //   .resize({ width: 200 }) // resize the image, forcing a width of 200 pixels. The height is computed automatically to preserve the aspect ratio.
+    //   .rotate(30); // rotate the image clockwise by 30 degrees.
+    value = image.grey().toDataURL();
+    console.log(value);
+  }
+
   return (
     <div className="app">
-      <h2>hello world</h2>
+      <img src="./1.jpg" />
+      <button onClick={execute()}>Click</button>
+      <img src={value} />
     </div>
   );
 }
