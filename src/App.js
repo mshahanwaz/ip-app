df889a3e2548e7ab02f44e693e6954f48d030867
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-03-18 00:10:33 +0530

    Added Runnable Code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
-import Image from "image-js";
-import { useEffect } from 'react';
+import "./App.css";
+import ImageDesign from "./ImageDesign";
 
 function App() {
-  var value;
-  // execute().catch(console.error);
-  
-  async function execute() {
-    let image = await Image.load('1.jpg');
-    // let grey = image
-    //   .grey() // convert the image to greyscale.
-    //   .resize({ width: 200 }) // resize the image, forcing a width of 200 pixels. The height is computed automatically to preserve the aspect ratio.
-    //   .rotate(30); // rotate the image clockwise by 30 degrees.
-    value = image.grey().toDataURL();
-    console.log(value);
-  }
-
   return (
     <div className="app">
-      <img src="./1.jpg" />
-      <button onClick={execute()}>Click</button>
-      <img src={value} />
+      <ImageDesign />
     </div>
   );
 }
---
+++ src/ImageDesign.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "image-js";
+import "./ImageDesign.css";
+import { useState } from "react";
+
+function ImageDesign() {
+  const [clicked, setClicked] = useState(true);
+
+  async function loadingImage() {
+    var image = await Image.load(document.getElementsByTagName("img")[0].src);
+    var grey = image.grey();
+    // console.log(grey);
+    setClicked(false);
+    document.getElementsByTagName("img")[1].src = grey.toDataURL();
+  }
+
+  return (
+    <div className="imageDesign">
+      <img
+        className="imageDesign__input"
+        src="https://cdn2.unrealengine.com/15br-mandoltm-social-newsheader-1920x1080-c670ddb2fea1.png"
+        alt="input"
+        width="500px"
+      />
+      <button onClick={() => loadingImage()}>Click</button>
+      <img
+        className={`imageDesign__output ${clicked && "imageDesign__hide"}`}
+        alt="output"
+        width="500px"
+      />
+    </div>
+  );
+}
+
+export default ImageDesign;
