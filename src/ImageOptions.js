0c35573d8b0c39ba266d0db5da8422a63a83f959
vinayak8295 <imvinayak1@gmail.com>
2021-03-18 23:17:50 +0530

    Update ImageOptions.js

--- src/ImageOptions.js
+++ src/ImageOptions.js
@@ -2,15 +2,29 @@ import React from "react";
 import Image from "image-js";
 
 function ImageOptions(props) {
+  
   const handleChange = async () => {
-    var image = await Image.load(props.source);
-    var grey = image.grey();
-    props.onOutputChange(grey.toDataURL());
+    var property = "grey";
+    switch(property) {
+      case "grey":
+        var image = await Image.load(props.source);
+        var grey = image.grey();
+        props.onOutputChange(grey.toDataURL());
+        break;
+        case "rotate":
+        var image = await Image.load(props.source);
+        var grey = image.rotate(60);
+        props.onOutputChange(grey.toDataURL());
+        break;
+      default:
+    }
+   
   };
 
   return (
     <div className="imageOptions">
       <button onClick={handleChange}>Grey</button>
+      {/* <button onClick={handleChange_grey}>Rotate</button> */}
     </div>
   );
 }
