bde3751d017c2fab93f54897db37d763995f85f5
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-03-19 17:33:04 +0530

    Added final code

--- src/ImageDesign.js
+++ src/ImageDesign.js
@@ -2,27 +2,35 @@ import React from "react";
 import "./ImageDesign.css";
 import { useState } from "react";
 import ImageOptions from "./ImageOptions";
+import bgimg from "./bgimg.png";
 
 function ImageDesign() {
   const [clicked, setClicked] = useState(false);
   const [image, setImage] = useState("");
   const [source, setSource] = useState("");
+  const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
-
-  //
+  const [downloadURL, setDownloadURL] = useState("");
 
   const handleChange = (e) => {
     if (e.target.files[0]) {
       setImage(e.target.files[0]);
+      document.getElementsByTagName("label")[0].innerText = `${
+        e.target.files[0].name
+      } ${(e.target.files[0].size / 1000).toFixed(2)}KB`;
     }
   };
 
   const handleUpload = () => {
-    setSource(URL.createObjectURL(image));
-    setClicked(true);
+    if (image) {
+      setSource(URL.createObjectURL(image));
+      setInput(URL.createObjectURL(image));
+      setClicked(true);
+    }
   };
 
   const outputChange = (outputValue) => {
+    setDownloadURL(outputValue);
     setOutput(outputValue);
   };
 
@@ -30,24 +38,37 @@ function ImageDesign() {
     <div className="imageDesign">
       {clicked ? (
         <div className="imageDesign__image">
-          <img id="imageDesign__input" src={source} alt="input" width="500px" />
+          <div className="imageDesign__input">
+            <p>original</p>
+            <img src={input} alt="input" />
+          </div>
           {output && (
-            <img
-              id={`imageDesign__output`}
-              src={output}
-              alt="output"
-              width="500px"
-            />
+            <div className="imageDesign__output">
+              <p>editted</p>
+              <img src={output} alt="output" />
+            </div>
           )}
         </div>
       ) : (
-        <div className="imageDesign__image">
-          <input type="file" accept="image/*" onChange={handleChange} />
+        <div className="imageDesign__upload">
+          <div className="imageDesign__inputFile">
+            <input
+              type="file"
+              id="imageDesign__fileUpload"
+              accept="image/*"
+              onChange={handleChange}
+            />
+            <label for="imageDesign__fileUpload">Select image</label>
+          </div>
           <button onClick={handleUpload}>Upload</button>
         </div>
       )}
       <div className="imageDesign__options">
-        <ImageOptions source={source} onOutputChange={outputChange} />
+        <ImageOptions
+          source={source}
+          onOutputChange={outputChange}
+          downloadURL={downloadURL}
+        />
       </div>
     </div>
   );
--- src/ImageOptions.js
+++ src/ImageOptions.js
@@ -1,16 +1,139 @@
-import React from "react";
 import Image from "image-js";
+import "./ImageOptions.css";
 
 function ImageOptions(props) {
-  const handleChange = async () => {
-    var image = await Image.load(props.source);
-    var grey = image.grey();
-    props.onOutputChange(grey.toDataURL());
-  };
+  async function doGrey() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var newImage = image.grey();
+    props.onOutputChange(newImage.toDataURL());
+  }
+
+  async function doRotate() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var angle = parseInt(window.prompt("Input angle(deg):", "0"));
+    if (angle) {
+      var newImage = image.rotate(angle);
+      props.onOutputChange(newImage.toDataURL());
+    }
+  }
+
+  async function doHFlip() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var newImage = image.flipX();
+    props.onOutputChange(newImage.toDataURL());
+  }
+
+  async function doVFlip() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var newImage = image.flipY();
+    props.onOutputChange(newImage.toDataURL());
+  }
+
+  async function doResize() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var width = parseInt(window.prompt("Input width(px): "));
+    var height = parseInt(window.prompt("Input height(px): "));
+    if (width && height) {
+      var newImage = image.resize({ width: width, height: height });
+      props.onOutputChange(newImage.toDataURL());
+    }
+  }
+
+  async function doInvert() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var newImage = image.invert();
+    props.onOutputChange(newImage.toDataURL());
+  }
+
+  async function doLighten() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var newImage = image.grey({
+      algorithm: "lightness",
+    });
+    props.onOutputChange(newImage.toDataURL());
+  }
+
+  async function doBlur() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var radius = parseInt(window.prompt("Input radius:"));
+    if (radius) {
+      var newImage = image.blurFilter({ radius: radius });
+      props.onOutputChange(newImage.toDataURL());
+    }
+  }
+
+  async function doCrop() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var x = parseInt(window.prompt("Input X:"));
+    var y = parseInt(window.prompt("Input Y:"));
+    var width = parseInt(window.prompt("Input width(px):"));
+    var height = parseInt(window.prompt("Input height(px):"));
+    if (x && y && width && height) {
+      var newImage = image.crop({
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+      });
+      props.onOutputChange(newImage.toDataURL());
+    }
+  }
+
+  async function doCyan() {
+    var image = await Image.load(
+      props.downloadURL ? props.downloadURL : props.source
+    );
+    var newImage = image.grey({
+      algorithm: "saturation",
+    });
+    props.onOutputChange(newImage.toDataURL());
+  }
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
   return (
     <div className="imageOptions">
-      <button onClick={handleChange}>Grey</button>
+      <p>Options</p>
+      <button onClick={doCrop}>Crop</button>
+      <button onClick={doResize}>Resize</button>
+      <button onClick={doRotate}>Rotate</button>
+      <button onClick={doHFlip}>H-Flip</button>
+      <button onClick={doVFlip}>V-Flip</button>
+      <button onClick={doLighten}>Grey</button>
+      <button onClick={doGrey}>Light Grey</button>
+      <button onClick={doCyan}>Cyan</button>
+      <button onClick={doInvert}>Invert</button>
+      <button onClick={doBlur}>Blur</button>
+      <button>
+        {props.downloadURL ? (
+          <a href={props.downloadURL} download="image69.png">
+            Download
+          </a>
+        ) : (
+          "Download"
+        )}
+      </button>
+      <button onClick={refreshPage}>Refresh</button>
     </div>
   );
 }
